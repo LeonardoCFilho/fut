dab43b279de1308298666ced4ac05a1c06f9f090
eadaianne <eadaianne@gmail.com>
2025-04-30 20:10:43 -0300

    sprint 5

---
+++ Frontend/Paginas/dashboard.py
@@ -0,0 +1,132 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import matplotlib.pyplot as plt
+
+
+def render():
+   
+    #para boas qtds de dados é bom usar
+    cores = {
+        'verde':'#008000',
+        'vermelho':'#800000',
+        'azul':'#4169E1'
+        }
+
+    @st.cache_data
+    def carregar_dados():
+        try:
+            dados = pd.read_csv('dados_2.csv', parse_dates=['data'])
+            return dados
+        except FileNotFoundError:
+            st.error("Arquivo 'dados.csv' não encontrado!")
+            return pd.DataFrame(columns=['data', 'total', 'acertos', 'erros'])
+
+    dados = carregar_dados()
+
+    # titulo da pagina
+    st.title("Dashboard de Casos de Teste")
+
+    # barra lateral para filtrar data
+    st.sidebar.header("Filtros")
+    data_inicial = st.sidebar.date_input("Data Inicial", dados['data'].min())
+    data_final = st.sidebar.date_input("Data Final", dados['data'].max())
+
+
+    # filtrando os dados 
+    dados_filtrados = dados[
+        (dados['data'] >= pd.to_datetime(data_inicial)) & 
+        (dados['data'] <= pd.to_datetime(data_final))
+    ]
+
+    # ********************************** #
+    st.title('Todas as Estatísticas')
+    # criando as 3 colunas iniciais
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        fig_totais = px.area(
+            dados_filtrados,
+            x='data',
+            y='total',
+            title='Testes Totais',
+            color_discrete_sequence=[cores['azul']],
+        )
+        st.plotly_chart(fig_totais, use_container_width=True)
+        st.metric("Total de Testes", dados_filtrados['total'].sum())
+
+    with col2:
+        fig_acertos = px.area(
+            dados_filtrados,
+            x='data',
+            y='acertos',
+            title='Acertos Totais',
+            color_discrete_sequence=[cores['verde']]
+        )
+        st.plotly_chart(fig_acertos, use_container_width=True)
+        st.metric("Acertos", dados_filtrados['acertos'].sum())
+
+    with col3:
+        fig_erros = px.area(
+            dados_filtrados,
+            x='data',
+            y='erros',
+            title='Erros Totais',
+            color_discrete_sequence=[cores['vermelho']]
+        )
+        st.plotly_chart(fig_erros, use_container_width=True)
+        st.metric("Erros", dados_filtrados['erros'].sum())
+
+    # *******************************
+
+    #declarando barras
+    chart_data = dados_filtrados[["total", "acertos", "erros"]]
+    st.bar_chart(chart_data, color=[cor for nome,cor in cores.items()])
+
+    ###
+    fig_barras = px.bar(dados_filtrados, 
+        x=dados_filtrados.index, 
+        y=["total", "acertos", "erros"],
+        barmode='group',
+        title="Desempenho dos Testes",
+        #"color_discrete_sequence= str([cores['azul'], [cores['verde']], [cores['vermelho']]]),
+    )
+    st.plotly_chart(fig_barras)
+
+    media_testes = dados_filtrados['total'].mean()
+
+    st.write(f"Média de testes por período: {media_testes:.2f}\n")
+
+    #tabela
+    st.subheader('Dados Filtrados')
+    st.dataframe(dados_filtrados)
+
+    # grafico de linha
+    st.subheader("Testes Executados por Período")
+    fig_linha = px.line(
+        dados_filtrados, 
+        x='data', 
+        y='total', 
+        markers=True,
+        labels={'data': 'Data', 'total': 'Total de Testes'}
+    )
+    st.plotly_chart(fig_linha)
+
+    # grafico de Pizza 
+    st.subheader("Distribuição de Acertos vs. Erros")
+    totais = dados_filtrados['total'].sum()
+    acertos_total = dados_filtrados['acertos'].sum()
+    erros_total = dados_filtrados['erros'].sum()
+
+    fig_pizza = px.pie(
+        names=['Acertos', 'Erros'],
+        values=[acertos_total, erros_total],
+        title='Taxa de Sucesso',
+        color_discrete_sequence=[cores['verde'], cores['vermelho']]
+    )
+    st.plotly_chart(fig_pizza)
+
+
+
+
+
+
---
+++ Frontend/Paginas/formulario.py
@@ -0,0 +1,64 @@
+import streamlit  as st 
+import yaml
+import os 
+
+def render():
+    st.title("Teste Manual")
+    #st.write("Conteúdo do formulário.")
+
+    if "ig_lista" not in st.session_state:
+        st.session_state.ig_lista = []
+    if "profile_lista" not in st.session_state:
+        st.session_state.profile_lista = []
+
+    #cuidado com o endereço, este é o do meu pc, modifique  ****
+    diretorio_destino_temp = r'C:\Users\DD\Desktop\Projetos\novo_streamlit\meu_app\temp'        
+    os.makedirs(diretorio_destino_temp, exist_ok=True)
+
+    st.header("Validação via Formulário Manual")
+    with st.form("formulario_manual"):
+            id = (st.text_input('ID*')).strip().lower() ##
+            descricao = st.text_input("Descrição (Opcional)")##
+            contexto = st.text_input("Contexto") ##
+            igs = st.text_input("IGs (Opcional)") ##
+            profiles = st.text_input("Profiles (Opcional)")##
+            resources = st.text_input("Resources (Opcional)")##
+            path_fhir = st.text_input("Path Fhir*")##
+            caminho_instancia = st.text_input("Caminho Instância*")##
+            default_validator = "caminho/ou/link/do/validador_padrao"
+            validator_fhir = st.text_input("Validator FHIR (Opcional):", value=default_validator)
+            botao_validar = st.form_submit_button("Criar Teste")
+
+    if botao_validar:
+        if not id or not path_fhir or not caminho_instancia or not validator_fhir:
+            st.error("Por favor, preencha ambos os campos obrigatórios.")
+        else:
+            if igs:
+                st.session_state.ig_lista.extend([ig.strip() for ig in igs.split(",") if ig.strip()])
+            if profiles:
+                st.session_state.profile_lista.extend([profile.strip() for profile in profiles.split(",") if profile.strip()])
+            
+            dados_teste = {
+            "test_id": id,
+            "description": descricao,
+            "context": contexto,
+            "igs": st.session_state.ig_lista,
+            "profiles": st.session_state.profile_lista,
+            "resources": [item.strip() for item in resources.split(",")] if resources else [],
+            "path_fhir": path_fhir,
+            "instance_path": caminho_instancia,
+            "validator": validator_fhir
+        }
+        
+            if f'{id}.yaml' in os.listdir(diretorio_destino_temp):
+                st.text('Arquivo já existe')
+            else:
+                try:
+                    caminho_arquivo = os.path.join(diretorio_destino_temp, f"{id}.yaml")
+                    with open(caminho_arquivo, "w", encoding="utf-8") as arquivo_yaml:
+                        yaml.dump(dados_teste, arquivo_yaml, allow_unicode=True)
+                    st.success(f"Arquivo salvo com sucesso em: {caminho_arquivo}")
+                    ...
+                except Exception as e:
+                    st.error(f'Erro ao salvar arquivo {e}')
+                    ...
